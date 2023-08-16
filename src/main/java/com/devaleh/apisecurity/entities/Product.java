ad4cf9d8c1e3665e2a9e4cc1e7d394776fe5d0b2
Alexandre Assuncao <alexandre.myemail@gmail.com>
2023-08-16 18:01:50 -0300

    commit test

---
+++ src/main/java/com/devaleh/apisecurity/entities/Product.java
@@ -0,0 +1,11 @@
+package com.devaleh.apisecurity.entities;
+
+import java.util.UUID;
+
+public class Product {
+
+    private UUID idProduct;
+    private String name;
+    private Double price;
+    
+}
