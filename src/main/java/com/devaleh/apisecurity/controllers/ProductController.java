e790fc6caa15f09d9e74a8ff9bfa9f744617f389
Alexandre Assuncao <alexandre.myemail@gmail.com>
2023-08-17 00:08:55 -0300

    added getAllProduct and getOneProduct method + expection

--- src/main/java/com/devaleh/apisecurity/controllers/ProductController.java
+++ src/main/java/com/devaleh/apisecurity/controllers/ProductController.java
@@ -2,12 +2,19 @@ package com.devaleh.apisecurity.controllers;
 
 import com.devaleh.apisecurity.dtos.ProductDto;
 import com.devaleh.apisecurity.entities.Product;
+import com.devaleh.apisecurity.repositories.ProductRepository;
 import com.devaleh.apisecurity.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 
 @RestController
 @RequestMapping(value = "/products")
@@ -20,6 +27,17 @@ public class ProductController {
     public ResponseEntity<Product> saveProduct(@RequestBody @Valid ProductDto productDto) {
         var product = new Product();
         BeanUtils.copyProperties(productDto, product);
-        return ResponseEntity.ok().body(service.insert(product));
+        return ResponseEntity.ok().body(service.save(product));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProduct() {
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> getOneProduct(@PathVariable UUID id) {
+        Product product = service.findById(id);
+        return ResponseEntity.ok().body(product);
     }
 }
--- src/main/java/com/devaleh/apisecurity/services/ProductService.java
+++ src/main/java/com/devaleh/apisecurity/services/ProductService.java
@@ -2,8 +2,15 @@ package com.devaleh.apisecurity.services;
 
 import com.devaleh.apisecurity.entities.Product;
 import com.devaleh.apisecurity.repositories.ProductRepository;
+import com.devaleh.apisecurity.services.exceptions.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ProductService {
@@ -11,7 +18,16 @@ public class ProductService {
     @Autowired
     ProductRepository repository;
 
-    public Product insert(Product product) {
+    public Product save(Product product) {
         return repository.save(product);
+    }
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(UUID id) {
+        Optional<Product> product = repository.findById(id);
+        return product.orElseThrow(() -> new NotFoundException(id));
     }
 }
