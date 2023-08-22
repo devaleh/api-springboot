0ec5d0176747a2c74d0e686b2c95ad24104933ca
Alexandre Assuncao <alexandre.myemail@gmail.com>
2023-08-22 17:10:06 -0300

    User repository and service

---
+++ src/main/java/com/devaleh/apisecurity/services/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.devaleh.apisecurity.services;
+
+import com.devaleh.apisecurity.entities.User;
+import com.devaleh.apisecurity.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " +username));
+        return user;
+    }
+}
